944a2c4efb73a869afa311f346885a7885792b55
Nilton Cezar <niltoncezar95@gmail.com>
2018-05-28 14:55:14 -0300

    Components cliente & os

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2018-si7-finalHomework
+
+#npm install -g @angular/cli
+
+#ng new ordemDeServicos
+
+#ng generate component cliente
+#ng generate component os
--- ordemDeServico/src/app/app.module.ts
+++ ordemDeServico/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { OsComponent } from './os/os.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    OsComponent
   ],
   imports: [
     BrowserModule
