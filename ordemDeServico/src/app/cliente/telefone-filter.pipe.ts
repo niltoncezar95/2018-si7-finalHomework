ba250b1308bf171d3536e7189456ea40a353ee9d
Nilton Cezar <niltoncezar95@gmail.com>
2018-06-08 02:48:20 -0300

    Mask campo telefone & detectando se tem 9 digitos

--- ordemDeServico/src/app/cliente/telefone-filter.pipe.ts
+++ ordemDeServico/src/app/cliente/telefone-filter.pipe.ts
@@ -5,14 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TelefoneFilterPipe implements PipeTransform {
 
-  transform(value: string, isAddDDD: boolean): string {
-    let newValue = '';
+  transform(value: string): string {
 
-    if (isAddDDD) {
-      newValue = '(35) ';
+    let is9Digit;
+    console.log(value.length)
+    if(value.length == 11 ) 
+      is9Digit = true;
+    
+    let newValue = '(';
+
+    if (is9Digit) {
+      newValue += value.substring(0, 2) + ') '
+                + value.substring(2, 7) + '-'
+                + value.substring(7);
+    } else{
+      newValue += value.substring(0, 2) + ') '
+                + value.substring(2, 6) + '-'
+                + value.substring(6);
     }
-    newValue += value.substring(0, 4) + '-' + value.substring(4);
-
     return newValue;
   }
 
